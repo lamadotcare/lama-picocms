c6e4f2746f958100f74efefa592baf6de638558c
padthaitofuhot <padthaitofuhot@users.noreply.github.com>
2020-08-17 00:22:07 -0400

    refactor gitup2date to pull before sleep

--- picocms/gitup2date.sh
+++ picocms/gitup2date.sh
@@ -5,13 +5,15 @@ if ! [ -f /usr/share/nginx/html/pico ]; then
   && curl -sSL https://getcomposer.org/installer | php \
   && git clone $PICO_COMPOSER_REPO pico \
   && php composer.phar --working-dir=pico install || exit 1
+  echo "Waiting ${GITUP2DATE_SLEEP:-900} seconds until update..."
+  sleep ${GITUP2DATE_SLEEP:-900}
 fi
 
 cd /usr/share/nginx/html/pico
 
 while true; do
-  echo "Waiting ${GITUP2DATE_SLEEP:-900} seconds until update..."
-  sleep ${GITUP2DATE_SLEEP:-900}
   git pull
   php ../composer.phar update
+  echo "Waiting ${GITUP2DATE_SLEEP:-900} seconds until update..."
+  sleep ${GITUP2DATE_SLEEP:-900}
 done
